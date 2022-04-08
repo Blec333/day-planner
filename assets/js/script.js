152b420cdd7fdd759653aaa78eda525c690b7623
BLEC333 <bleclair3@gmail.com>
2022-04-07 21:35:01 -0700

    complete storage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,32 @@ function displayTime() {
     secondsLeftToday = (((24 - hourNow) * 60 * 60) - (minNow * 60) - secNow);
     $('#currentDay').text(rightNow);
     window.setTimeout("displayTime()", 1000);
-    console.log(secondsLeftToday);
+}
+
+
+//Get storage
+function retrieveStoredArray(storedDataName) {
+    const forceArray = (v) => [].concat(v).map(name => name);
+    var storedArray = forceArray(JSON.parse(localStorage.getItem(storedDataName)));
+    return storedArray;
+}
+
+//Update storage
+function updateStoredArray(storedDataName, addData) {
+    const forceArray = (v) => [].concat(v).map(name => name);
+    var storedArray = JSON.parse(localStorage.getItem(storedDataName));
+    if (typeof storedArray === 'string' && storedArray.length > 0) {
+        storedArray = forceArray(JSON.parse(localStorage.getItem(storedDataName)));
+        var combinedArray = storedArray.push(addData);
+        var backToStorage = JSON.stringify(combinedArray);
+    } else if (typeof storedarray === 'object') {
+        var combinedArray = storedArray.push(addData);
+        var backToStorage = JSON.stringify(combinedArray);
+    } else {
+        var backToStorage = JSON.stringify(forceArray(addData));
+    }
+    localStorage.setItem(storedDataName, backToStorage);
+    return console.log('Stored ' + storedDataName + ' as: ' + backToStorage)
 }
 
 //DEFINE UTILITY FUNCTIONS ABOVE
@@ -37,9 +62,10 @@ function displayTime() {
 
 function createTimeblocks() {
     //populate the content
-        for (var i = 0; i < 9; i++) {
+            var hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
+        for (var i = 0; i < hours.length; i++) {
 
-            secondsToEOD = ((24 - (i + 9)) * 60 * 60);
+            secondsToEOD = ((24 - (i + hours.length)) * 60 * 60);
             if (secondsLeftToday < (secondsToEOD) && secondsLeftToday > (secondsToEOD - 1200)) {
                var blockColorClass = 'present';
             } else if (secondsLeftToday > secondsToEOD) {
@@ -48,19 +74,15 @@ function createTimeblocks() {
                 var blockColorClass = 'past';
             }
 
-            var hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
             var createTimeBlock = $('<div>').addClass('time-block row d-flex col-12').attr('id', 'timeblock' + i);
             var createHour = $('<div>').addClass('hour col').text(hours.at(i));
-            var createTextInput = $('<input>').addClass('textarea ' + blockColorClass + ' col-8');
-            createSaveButton = $('<button>').addClass('saveBtn col');
+            var createTextInput = $('<input>').addClass('textarea ' + blockColorClass + ' col-8')
+            var addStoredText = createTextInput.val(retrieveStoredArray(createHour.text()+'-Data').at(0));
+            createSaveButton = $('<button>').addClass('saveBtn col').attr('id', 'button' + i);
             $('.container').append(createTimeBlock);
             createTimeBlock.append(createHour, createTextInput, createSaveButton);
-            
-    console.log(secondsToEOD);
-    console.log(blockColorClass);
         }
     }
-
 
 
 
@@ -77,7 +99,16 @@ displayTime();
 //LISTEN AND TAKE ACTION BELOW
 
 
-
+document.querySelectorAll('.saveBtn').addEventListener('click', function (event) {
+    var btnEl = $(event.target);
+    activatedTimeBlock = btnEl.parent();
+    var targetedText = activatedTimeBlock.children('.textarea').val();
+    console.log(targetedText);
+    var hourText = activatedTimeBlock.children('.hour').text() + '-Data';
+    console.log(hourText);
+    updateStoredArray(hourText, targetedText);
+    console.log(targetedText);
+});
 
 
 
